6071fc0dd0b26d34759ee396936f0174600a8d38
Darshan99225 <41063815+Darshan99225@users.noreply.github.com>
2025-10-05 12:40:00 +0200

    changes CMAKE

--- setup.py
+++ setup.py
@@ -6,11 +6,17 @@ setup(
     name=package_name,
     version='0.1.0',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/my_launch.py']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Student',
     maintainer_email='student@example.com',
-    description='Simple robot behaviour package',
+    description='Simple robot behaviour package (obstacle avoidance + FSM)',
     license='MIT',
     entry_points={
         'console_scripts': [
