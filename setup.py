f51ec43f824d78bd2f9daaa43c5c78db7281806d
Darshan99225 <41063815+Darshan99225@users.noreply.github.com>
2025-10-05 11:56:25 +0200

    rename

--- setup.py
+++ setup.py
@@ -6,21 +6,17 @@ setup(
     name=package_name,
     version='0.1.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Student',
     maintainer_email='student@example.com',
-    description='Simple robot behaviour package (obstacle avoidance + FSM)',
+    description='Simple robot behaviour package',
     license='MIT',
     entry_points={
         'console_scripts': [
             'obstacle_avoidance = robot_behaviour.obstacle_avoidance:main',
             'fsm_controller = robot_behaviour.fsm_controller:main',
-            'teleop_simple = robot_behaviour.teleop_simple:main'
+            'teleop_simple = robot_behaviour.teleop_simple:main',
         ],
     },
 )
